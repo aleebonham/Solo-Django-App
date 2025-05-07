7f6fa5b2fd11e6d63cd980015b30819a4a1588cc
CLORA BONHAM <c.bonham.24@abdn.ac.uk>
2025-05-07 23:41:53 +0000

    Added books app, model, detail views, and templates

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=15)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    query = request.GET.get('q')
+    if query:
+        books = books.filter(title__icontains=query) | books.filter(author__icontains=query) | books.filter(genre__icontains=query)
+    return render(request, 'books/books_list.html', {'books' : books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_detail.html', {'book': book})
